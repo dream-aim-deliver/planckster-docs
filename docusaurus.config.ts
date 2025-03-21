290c209966cd3ea7886c282a058885c9c74fe156
maany <imptodefeat@gmail.com>
2025-03-21 14:53:46 +0100

    Update production URL in docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,8 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://dream-aim-deliver.github.io/',
+  // url: 'https://dream-aim-deliver.github.io/',
+  url: 'https://skypulse-docs.devmaany.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   // baseUrl: '/planckster-docs/',
