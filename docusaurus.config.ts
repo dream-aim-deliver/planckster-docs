835d348b699ff516cd7c8b5c133ae9b6bdbfddb9
maany <imptodefeat@gmail.com>
2025-03-21 14:48:44 +0100

    Update baseUrl in docusaurus.config.ts for deployment configuration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,8 @@ const config: Config = {
   url: 'https://dream-aim-deliver.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/planckster-docs/',
+  // baseUrl: '/planckster-docs/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
